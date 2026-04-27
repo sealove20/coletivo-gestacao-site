02fdd1186942f459cb05e0de96b0653520ab25df
Matheus Henrique <matheus.181137@gmail.com>
2026-04-27 16:39:30 -0300

    fix: switch Keystatic storage to cloud mode to remove env var requirement

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -2,8 +2,7 @@ import { config, fields, collection } from "@keystatic/core";
 
 export default config({
   storage: {
-    kind: "github",
-    repo: "sealove20/coletivo-gestacao-site",
+    kind: "cloud",
   },
   cloud: {
     project: "devkunn/coletivo-gestacao",
